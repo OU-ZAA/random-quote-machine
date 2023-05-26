f09f4da0352058c042de7875861712b89c134550
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-05-26 23:05:53 +0100

    add the ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <>
-      <div>Hello world</div>
+      <div className="grid place-items-center h-screen bg-slate-900">
+        <div className="w-[500px] bg-slate-100 p-6">
+          <p className="text-2xl">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates exercitationem atque eaque tempore 
+            adipisci, pariatur fuga voluptate ex molestiae, qui fugiat quia? Reprehenderit consequuntur ad odit, quam 
+            magni dolorem rem?
+          </p>
+          <p className="text-right text-lg pb-2">Victor Hugo</p>
+          <div className="flex justify-between">
+            <button className="p-3 rounded-md bg-slate-900">
+              <FaTwitter className="text-white" />
+            </button>
+            <button className="p-2 rounded-md bg-slate-900 text-white">
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
