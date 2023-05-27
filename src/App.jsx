6a2393479f447eafabe6eee3673352ea16ab3111
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-05-27 23:20:55 +0100

    fetch the data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,55 @@
-import {FaTwitter} from 'react-icons/fa'
+import { useEffect, useState } from 'react'
+import { FaTwitter } from 'react-icons/fa'
 
 function App() {
-
   return (
     <>
-      <div className="grid place-items-center h-screen bg-slate-900">
-        <div className="w-[500px] bg-slate-100 p-6">
-          <p className="text-2xl">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates exercitationem atque eaque tempore 
-            adipisci, pariatur fuga voluptate ex molestiae, qui fugiat quia? Reprehenderit consequuntur ad odit, quam 
-            magni dolorem rem?
-          </p>
-          <p className="text-right text-lg pb-2">Victor Hugo</p>
-          <div className="flex justify-between">
-            <button className="p-3 rounded-md bg-slate-900">
-              <FaTwitter className="text-white" />
-            </button>
-            <button className="p-2 rounded-md bg-slate-900 text-white">
-              New Quote
-            </button>
-          </div>
+      <QuoteBox />
+    </>
+  )
+}
+
+function QuoteBox() {
+  const [quote, setQuote] = useState({})
+
+  function fetchData() {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(data => setQuote(data))
+  }
+  
+  function handleClick() {
+    fetchData()
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div className="grid place-items-center h-screen bg-slate-900">
+      <div className="w-[500px] bg-slate-100 p-6 rounded-lg">
+        <p className="text-2xl">
+          {quote.content}
+        </p>
+        <p className="text-right text-lg pb-2">~ {quote.author}</p>
+        <div className="flex justify-between">
+          <a 
+            className="p-3 rounded-md bg-slate-900"
+            target="_top"
+            href={`https://twitter.com/intent/tweet?text="${quote.content}" ${quote.author}`}
+          >
+            <FaTwitter className="text-white" />
+          </a>
+          <button 
+            onClick={handleClick}
+            className="p-2 rounded-md bg-slate-900 text-white"
+          >
+            New Quote
+          </button>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
